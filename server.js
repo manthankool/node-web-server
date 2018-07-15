7d276b3582bfa33456fce27967de872770ea4890
Manthan <manthankoolwal2450@gmail.com>
2018-07-15 14:46:44 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+
+hbs.registerPartials(__dirname + '/views/partials')//to add support for partials
+app.set('view engine','hbs')                      //this will allow us to set express related configuration;
+
+
+
+app.use((req,res,next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err){
+      console.log('Unable to load logs to the file');
+    }
+ });
+  next();
+
+});
+
+// app.use((req,res,next) => {
+//   res.render('maintinance.hbs');
+//
+// });
+
+app.use(express.static(__dirname + '/public'));  //absolute directory
+
+hbs.registerHelper('getCurrentYear',() => {          //helpers can also take arguements
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt',(text) => {
+  return text.toUpperCase();
+});
+
+app.get('/',(req,res) => {
+  // res.send('<h1>hello fucker</h1>');
+  res.render('home.hbs',{
+    pageTitle:'Home Page',
+    welcomeMessage:'Come and fucked by me and get rewarded and get my dildo too! and give your pussy to me'
+  });
+
+
+
+});
+
+app.get('/about',(req,res) => {
+
+  res.render('about.hbs',{
+    pageTitle:'About Page'
+  });
+
+
+});
+
+app.get('/bad',(req,res) => {
+  res.send({
+    errorMessage:'Unable to lead you to anything fucker'
+
+
+  });
+
+
+});
+
+app.listen(3000, () => {
+
+  console.log('Server is up on port 3000');
+});
